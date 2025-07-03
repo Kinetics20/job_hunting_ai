40fcb8b65f53623283773869b0ce5232066777d9
Kinetics20 <kinetics20@gmail.com>
2025-07-03 17:06:34 +0200

    practising alembic

--- microservices/personal_info/api/alembic/versions/d1fa4eebeda4_something.py
+++ microservices/personal_info/api/alembic/versions/d1fa4eebeda4_something.py
@@ -13,7 +13,7 @@ import sqlalchemy as sa
 
 # revision identifiers, used by Alembic.
 revision: str = 'd1fa4eebeda4'
-down_revision: Union[str, Sequence[str], None] = '929aa6e30fe9'
+down_revision: Union[str, Sequence[str], None] = None
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
